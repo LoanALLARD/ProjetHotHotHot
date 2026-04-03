e5a2c3dc0fe17fd9f467b049b27cc2f96502dcd3
Loan <loan.allard@etu.univ-amu.fr>
2026-04-03 10:48:46 +0200

    refactor: Improve formatting and structure of TabsManual class

--- public/js/app.js
+++ public/js/app.js
@@ -1,153 +1,151 @@
 "use strict";
 
 class TabsManual {
-    // Constructeur
-    constructor(groupNode) {
-        this.tablistNode = groupNode;
+  // Constructeur
+  constructor(groupNode) {
+    this.tablistNode = groupNode;
 
-        this.tabs = [];
+    this.tabs = [];
 
-        this.firstTab = null;
-        this.lastTab = null;
+    this.firstTab = null;
+    this.lastTab = null;
 
-        this.tabs = Array.from(this.tablistNode.querySelectorAll("[role=tab]"));
-        this.tabpanels = [];
+    this.tabs = Array.from(this.tablistNode.querySelectorAll("[role=tab]"));
+    this.tabpanels = [];
 
-        // Initialisation des tabs
-        for (var i = 0; i < this.tabs.length; i += 1) {
-            var tab = this.tabs[i];
-            var tabpanel = document.getElementById(
-                tab.getAttribute("aria-controls"),
-            );
+    // Initialisation des tabs
+    for (var i = 0; i < this.tabs.length; i += 1) {
+      var tab = this.tabs[i];
+      var tabpanel = document.getElementById(tab.getAttribute("aria-controls"));
 
-            tab.tabIndex = -1;
-            tab.setAttribute("aria-selected", "false");
-            this.tabpanels.push(tabpanel);
+      tab.tabIndex = -1;
+      tab.setAttribute("aria-selected", "false");
+      this.tabpanels.push(tabpanel);
 
-            tab.addEventListener("keydown", this.onKeydown.bind(this));
-            tab.addEventListener("click", this.onClick.bind(this));
+      tab.addEventListener("keydown", this.onKeydown.bind(this));
+      tab.addEventListener("click", this.onClick.bind(this));
 
-            if (!this.firstTab) {
-                this.firstTab = tab;
-            }
-            this.lastTab = tab;
-        }
-
-        this.setSelectedTab(this.firstTab);
+      if (!this.firstTab) {
+        this.firstTab = tab;
+      }
+      this.lastTab = tab;
     }
 
-    // Sélectionne le tab actuel
-    setSelectedTab(currentTab) {
-        for (var i = 0; i < this.tabs.length; i += 1) {
-            var tab = this.tabs[i];
-            if (currentTab === tab) {
-                tab.setAttribute("aria-selected", "true");
-                tab.removeAttribute("tabindex");
-                this.tabpanels[i].classList.remove("is-hidden");
-            } else {
-                tab.setAttribute("aria-selected", "false");
-                tab.tabIndex = -1;
-                this.tabpanels[i].classList.add("is-hidden");
-            }
-        }
+    this.setSelectedTab(this.firstTab);
+  }
+
+  // Sélectionne le tab actuel
+  setSelectedTab(currentTab) {
+    for (var i = 0; i < this.tabs.length; i += 1) {
+      var tab = this.tabs[i];
+      if (currentTab === tab) {
+        tab.setAttribute("aria-selected", "true");
+        tab.removeAttribute("tabindex");
+        this.tabpanels[i].classList.remove("is-hidden");
+      } else {
+        tab.setAttribute("aria-selected", "false");
+        tab.tabIndex = -1;
+        this.tabpanels[i].classList.add("is-hidden");
+      }
+    }
+  }
+
+  // Tab actuel
+  moveFocusToTab(currentTab) {
+    currentTab.focus();
+  }
+
+  // Déplacement au tab précédent
+  moveFocusToPreviousTab(currentTab) {
+    var index;
+
+    if (currentTab === this.firstTab) {
+      this.moveFocusToTab(this.lastTab);
+    } else {
+      index = this.tabs.indexOf(currentTab);
+      this.moveFocusToTab(this.tabs[index - 1]);
+    }
+  }
+
+  // Déplacement au tab suivant
+  moveFocusToNextTab(currentTab) {
+    var index;
+
+    if (currentTab === this.lastTab) {
+      this.moveFocusToTab(this.firstTab);
+    } else {
+      index = this.tabs.indexOf(currentTab);
+      this.moveFocusToTab(this.tabs[index + 1]);
+    }
+  }
+
+  // Handlers
+  onKeydown(event) {
+    var tgt = event.currentTarget,
+      flag = false;
+
+    switch (event.key) {
+      case "ArrowLeft":
+        this.moveFocusToPreviousTab(tgt);
+        flag = true;
+        break;
+
+      case "ArrowRight":
+        this.moveFocusToNextTab(tgt);
+        flag = true;
+        break;
+
+      case "Home":
+        this.moveFocusToTab(this.firstTab);
+        flag = true;
+        break;
+
+      case "End":
+        this.moveFocusToTab(this.lastTab);
+        flag = true;
+        break;
+
+      default:
+        break;
     }
 
-    // Tab actuel
-    moveFocusToTab(currentTab) {
-        currentTab.focus();
+    if (flag) {
+      event.stopPropagation();
+      event.preventDefault();
     }
+  }
 
-    // Déplacement au tab précédent
-    moveFocusToPreviousTab(currentTab) {
-        var index;
-
-        if (currentTab === this.firstTab) {
-            this.moveFocusToTab(this.lastTab);
-        } else {
-            index = this.tabs.indexOf(currentTab);
-            this.moveFocusToTab(this.tabs[index - 1]);
-        }
-    }
-
-    // Déplacement au tab suivant
-    moveFocusToNextTab(currentTab) {
-        var index;
-
-        if (currentTab === this.lastTab) {
-            this.moveFocusToTab(this.firstTab);
-        } else {
-            index = this.tabs.indexOf(currentTab);
-            this.moveFocusToTab(this.tabs[index + 1]);
-        }
-    }
-
-    // Handlers
-    onKeydown(event) {
-        var tgt = event.currentTarget,
-            flag = false;
-
-        switch (event.key) {
-            case "ArrowLeft":
-                this.moveFocusToPreviousTab(tgt);
-                flag = true;
-                break;
-
-            case "ArrowRight":
-                this.moveFocusToNextTab(tgt);
-                flag = true;
-                break;
-
-            case "Home":
-                this.moveFocusToTab(this.firstTab);
-                flag = true;
-                break;
-
-            case "End":
-                this.moveFocusToTab(this.lastTab);
-                flag = true;
-                break;
-
-            default:
-                break;
-        }
-
-        if (flag) {
-            event.stopPropagation();
-            event.preventDefault();
-        }
-    }
-
-    onClick(event) {
-        this.setSelectedTab(event.currentTarget);
-    }
+  onClick(event) {
+    this.setSelectedTab(event.currentTarget);
+  }
 }
 
 // Initialisation du tablist
 window.addEventListener("load", function () {
-    var tablists = document.querySelectorAll("[role=tablist].manual");
-    for (var i = 0; i < tablists.length; i++) {
-        new TabsManual(tablists[i]);
-    }
+  var tablists = document.querySelectorAll("[role=tablist].manual");
+  for (var i = 0; i < tablists.length; i++) {
+    new TabsManual(tablists[i]);
+  }
 });
 
 // Installation de l'application en tant que PWA
 window.addEventListener("beforeinstallprompt", (e) => {
-    e.preventDefault();
-    deferredPrompt = e;
-    installBtn.style.display = "block";
+  e.preventDefault();
+  deferredPrompt = e;
+  installBtn.style.display = "block";
 });
 
 // Écouteur pour le bouton d'installation
 installBtn.addEventListener("click", () => {
-    if (deferredPrompt) {
-        deferredPrompt.prompt();
-        deferredPrompt.userChoice.then((choiceResult) => {
-            if (choiceResult.outcome === "accepted") {
-                console.log("User accepted the A2HS prompt");
-            } else {
-                console.log("User dismissed the A2HS prompt");
-            }
-            deferredPrompt = null;
-        });
-    }
+  if (deferredPrompt) {
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === "accepted") {
+        console.log("User accepted the A2HS prompt");
+      } else {
+        console.log("User dismissed the A2HS prompt");
+      }
+      deferredPrompt = null;
+    });
+  }
 });
